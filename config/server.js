fa47e54201e5135a8838f62c406cd1760fb55a54
Alban Bleicher <alban.b@thewalkingnerds.com>
2021-02-13 12:32:00 +0100

    🎉  Begin a project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5e624a2d0e1a24bc5bc150693232e292'),
+    },
+  },
+});
