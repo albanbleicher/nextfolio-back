fc71b0bf4237041a7f34c7c83830e1b3b7a6b1df
Alban Bleicher <alban.b@thewalkingnerds.com>
2021-10-31 12:03:31 +0100

    🐛 make slugify more strict

--- api/projects/models/projects.js
+++ api/projects/models/projects.js
@@ -1,15 +1,15 @@
-const slugify = require('slugify');
+const slugify = require("slugify");
 
 module.exports = {
   lifecycles: {
     beforeCreate: async (data) => {
       if (data.title) {
-        data.slug = slugify(data.title, {lower: true});
+        data.slug = slugify(data.title, { lower: true, strict: false });
       }
     },
     beforeUpdate: async (params, data) => {
       if (data.title) {
-        data.slug = slugify(data.title, {lower: true});
+        data.slug = slugify(data.title, { lower: true, strict: false });
       }
     },
   },
