e3ffa6d60c0b2aec35b41bc827e937e177b7a309
Alban Bleicher <alban.b@thewalkingnerds.com>
2021-10-31 13:54:58 +0100

    🐛 Fix slug generation

--- api/projects/models/projects.js
+++ api/projects/models/projects.js
@@ -1,15 +1,22 @@
 const slugify = require("slugify");
+slugify.extend({ "'": "-" });
 
 module.exports = {
   lifecycles: {
     beforeCreate: async (data) => {
       if (data.title) {
-        data.slug = slugify(data.title, { lower: true, strict: false });
+        data.slug = slugify(data.title, {
+          lower: true,
+          strict: true,
+        });
       }
     },
     beforeUpdate: async (params, data) => {
       if (data.title) {
-        data.slug = slugify(data.title, { lower: true, strict: false });
+        data.slug = slugify(data.title, {
+          lower: true,
+          strict: true,
+        });
       }
     },
   },
